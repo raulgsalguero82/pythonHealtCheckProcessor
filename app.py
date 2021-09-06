1d2a0ba14b1415f706378673ffecaad1a6d90c84
Raul Garcia Salguero <r.garcia3@uniandes.edu.co>
2021-09-05 21:22:47 -0500

    * Task scheduler added

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+from flask_apscheduler import APScheduler
+from datetime import datetime
+import redis
 from flask import request
 from flask_restful import Resource,Api
 from flask import Flask
 from typing import Dict
 from flask import Flask
+
+
 
 
 def create_app(config_dict: Dict = {}):
@@ -13,11 +18,22 @@ class HealthCheck(Resource):
 
     def get(self):
         data={
-            "echo" : "ok"
+            "StartTime" : str(redisInstance.get("StartTime")),
+            "LastQuery" : str(redisInstance.get("LastQuery"))
         }
         return data
 
 
+redisInstance = redis.Redis(
+    host='ec2-52-73-185-231.compute-1.amazonaws.com', 
+    port=17030,
+    password="p8246bd54e4335f5d4001090409c247e242ebbc0d28a3a9a8f92400e7b9e1d178",
+    ssl=True,
+    ssl_cert_reqs=None
+    )
+
+startTime=datetime.now()
+redisInstance.set('StartTime', str(startTime))
 
 app = create_app()
 app_context = app.app_context()
@@ -26,6 +42,14 @@ app_context.push()
 api = Api(app)
 api.add_resource(HealthCheck, "/healthcheck")
 
+scheduler = APScheduler()
+
+@scheduler.task('interval', id='do_job_1', seconds=30, misfire_grace_time=900)
+def job1():
+    LastQuery=datetime.now()
+    redisInstance.set('LastQuery', str(LastQuery))    
+
+scheduler.start()
 
 
 if __name__ == '__main__':
